c7a05446cceb9bf2392b0fc29430103d7426e2e0
Ramachandran T <tramachandran1@gmail.com>
2020-07-09 20:18:29 +0530

    1) Added service for user validation 2) Added screenshots 3) Added user validation on login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
@@ -11,7 +12,8 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { LoginService } from '../services/login.service';
 
 interface UserModel {
   email: string,
@@ -10,15 +11,35 @@ interface UserModel {
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(private _loginService: LoginService) { }
   userModel: UserModel = {
     email: '',
     password: ''
   };
-  
-  ngOnInit(): void {
+
+  loginMsg: string = '';
+  errorMsg: string = '';
+
+  loginUser(): void {
+    this._loginService.validateUser().subscribe(
+      (res) => {
+        this.validateUserDetails(res.data);
+      },
+      (err) => {
+        this.errorMsg = 'Error occured on server';
+      })
   }
 
+  validateUserDetails(usersList: UserModel[]) {    
+    const matchedUser = usersList.find((user) => {
+      return user.email === this.userModel.email
+    });
+    if (matchedUser && matchedUser.password === this.userModel.password) {
+      this.loginMsg = "Login is successful....";
+    } else {
+      this.errorMsg = 'Invalid credentails';
+    }
+  }
 }
